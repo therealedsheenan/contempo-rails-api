a92d2bc9ef66363b39ba46a96fbbcd30021a56e6
Sheenan Tenepre <sheenantenepre@gmail.com>
2017-04-13 17:44:28 +0900

    add model

---
+++ db/migrate/20170413083754_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password
+      t.string :string
+      t.boolean :admin
+
+      t.timestamps
+    end
+  end
+end
