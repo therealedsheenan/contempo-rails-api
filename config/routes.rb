91088598350034703ae69a99f61ea1df3d7276a7
Sheenan Tenepre <sheenantenepre@gmail.com>
2017-04-19 12:28:57 +0900

    add users and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api do
+    post 'signin', to: 'authentication#authenticate'
     get 'users', to: :show, controller: 'user'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413083754) do
+ActiveRecord::Schema.define(version: 20170419031333) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "password"
-    t.boolean  "admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
